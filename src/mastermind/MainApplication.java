d975887c749c5e3f7c16e37cc64b26fd04f3b5c4
5ia14siaebersold <5ia14siaebersold@INFP42-43.gibwintra.ch>
2016-11-08 09:06:33 +0100

    first commit

--- src/mastermind/MainApplication.java
+++ src/mastermind/MainApplication.java
@@ -1,10 +1,14 @@
 package mastermind;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class MainApplication {
 	
 	private int[] numberRow = new int[4];
+	private int[] userNumberRow = new int[4];
+	private int correctPositions = 0;
+	private int correctNumbers = 0;
 	
 	public void generateNumberRow() {
 		
@@ -20,9 +24,84 @@ public class MainApplication {
 		
 	}
 	
+	public void getNumberRow(){
+		Scanner sc = new Scanner(System.in);
+		int numbers = sc.nextInt();
+
+		String number = String.valueOf(numbers);
+		for(int i = 0; i < number.length(); i++) {
+		    int j = Character.digit(number.charAt(i), 10);
+		    userNumberRow[i] = j;
+		}
+		
+		checkRow();
+		
+		//System.out.println(userNumberRow[0]+""+userNumberRow[1]+""+userNumberRow[2]+""+userNumberRow[3]);
+		
+	}
+	
+	public void checkRow() {
+		
+		correctPositions = 0;
+		correctNumbers = 0;
+	
+		if (userNumberRow[0] == numberRow[0]){
+			correctPositions = correctPositions + 1;
+		} else if(userNumberRow[0] == numberRow[1]) {
+			correctNumbers = correctNumbers + 1;
+		} else if(userNumberRow[0] == numberRow[2]) {
+			correctNumbers = correctNumbers + 1;
+		} else if(userNumberRow[0] == numberRow[3]) {
+			correctNumbers = correctNumbers + 1;
+		}
+		
+		if (userNumberRow[1] == numberRow[1]){
+			correctPositions = correctPositions + 1;
+		} else if(userNumberRow[1] == numberRow[0]) {
+			correctNumbers = correctNumbers + 1;
+		} else if(userNumberRow[1] == numberRow[2]) {
+			correctNumbers = correctNumbers + 1;
+		} else if(userNumberRow[1] == numberRow[3]) {
+			correctNumbers = correctNumbers + 1;
+		}
+		
+		if (userNumberRow[2] == numberRow[2]){
+			correctPositions = correctPositions + 1;
+		} else if(userNumberRow[2] == numberRow[0]) {
+			correctNumbers = correctNumbers + 1;
+		} else if(userNumberRow[2] == numberRow[1]) {
+			correctNumbers = correctNumbers + 1;
+		} else if(userNumberRow[2] == numberRow[3]) {
+			correctNumbers = correctNumbers + 1;
+		}
+		
+		if (userNumberRow[3] == numberRow[3]){
+			correctPositions = correctPositions + 1;
+		} else if(userNumberRow[3] == numberRow[0]) {
+			correctNumbers = correctNumbers + 1;
+		} else if(userNumberRow[3] == numberRow[1]) {
+			correctNumbers = correctNumbers + 1;
+		} else if(userNumberRow[3] == numberRow[2]) {
+			correctNumbers = correctNumbers + 1;
+		}
+		
+		if (correctPositions == 4) {
+			System.out.println("Congratulations! You won! :)");
+		} else {
+			System.out.println("correct Positions & Numbers: "+ correctPositions);
+			System.out.println("correct Numbers: "+ correctNumbers);
+
+			getNumberRow();
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		MainApplication main = new MainApplication();
 		main.generateNumberRow();
+		System.out.println("Geben Sie 4 Zahlen zwischen 1 & 6 ein:");
+		main.getNumberRow();
+		
 	}
 	
 }
